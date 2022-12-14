6674fae5e9bc970dd1b9422fdbcfb3dbdb72aea8
Vlad Papikyan <vladqot@icloud.com>
2022-12-14 03:39:13 +0300

    commit

---
+++ src/chapter2/IfSample.java
@@ -0,0 +1,26 @@
+package chapter2;
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x, y;
+        x = 10;
+        y = 20;
+
+        if (x < y) {
+            System.out.println("x is less than y");
+        }
+            x = x * 2;
+            if (x == y){
+                System.out.println("x now equal to y");
+            }
+            x = x * 2;
+            if (x > y){
+                System.out.println("x now greater than y");
+            }
+           //this wont display anything
+        if (x == y){
+            System.out.println("you wont see this");
+        }
+    }
+}
+
---
+++ src/chapter3/Scope.java
@@ -0,0 +1,16 @@
+package chapter3;
+
+public class Scope {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x == 10){
+            int y = 20;
+            System.out.println("x and y: " + x + " " +y);
+            x = y * 2;
+        }
+        // y = 100; // Error! y not known here
+
+        // x is still known here
+        System.out.println("x is " + x);
+    }
+}
