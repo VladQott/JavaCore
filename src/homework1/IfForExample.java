4aee5e93763f521b96ef480b95530be12a9d3a81
Vlad Papikyan <vladqot@icloud.com>
2022-12-19 15:07:03 +0300

    commit

--- src/homework1/IfForExample.java
+++ src/homework1/IfForExample.java
@@ -6,7 +6,11 @@ public class IfForExample {
         int b = 43;
         if (a < b) {
             System.out.println("max: " + b);
-        }
+        } else if (a > b) {
+            System.out.println("max:" + a);
+        } else
+            System.out.println("a is equal to b");
+
         for (a = 32; a <= b; a++) {
             System.out.print(a + ", ");
         }
