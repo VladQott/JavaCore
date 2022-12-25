d648bcbfbb770748226160e163b245b2a2df21ac
Vlad Papikyan <vladqot@icloud.com>
2022-12-25 13:03:37 +0300

    Array Homework

--- src/homework/homework2/ArrayUtil.java
+++ src/homework/homework2/ArrayUtil.java
@@ -2,7 +2,8 @@ package homework.homework2;
 
 public class ArrayUtil {
     public static void main(String[] args) {
-        int[] array = {3, 5, 34, 13, 33, 6, 78, 46, 2, 8,};
+
+        int[] array = {3, 5, 34, 13, 6, 33, 78, 46, 2, 8, 12};
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
@@ -23,5 +24,46 @@ public class ArrayUtil {
             }
         }
         System.out.println("Min nam is: " + minNum);
+        System.out.println();
+
+
+        System.out.println();
+        int evenCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+                evenCount++;
+            }
+        }
+        System.out.println();
+        System.out.println("Even count: " + evenCount);
+
+        int oddCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                System.out.print(array[i] + " ");
+                oddCount++;
+            }
+        }
+        System.out.println();
+        System.out.println("Odd count: " + oddCount);
+
+        double result = 0;
+        for (int i = 0; i < array.length; i++) {
+            result += array[i];
+        }
+        System.out.println("Average is: " + result / array.length);
+
+        int numSum = 0;
+        for (int i = 0; i < array.length; i++) {
+            numSum += array[i];
+        }
+        System.out.println("Summary is: " + numSum);
+
+        for (int i = 0; i < array.length; i++) {
+        }
+        System.out.println(array[0]);
+        System.out.println(array[array.length - 1]);
+        System.out.println(array[array.length / 2]);
     }
 }
