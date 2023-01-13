0779018d0a166c3c72b4a02e1e833e6175f0030a
Vlad Papikyan <vladqot@icloud.com>
2023-01-13 20:23:07 +0300

    Chapter6 Examples

---
+++ src/chapter6/Box.java
@@ -0,0 +1,12 @@
+package chapter6;
+
+public class Box {
+    double width;
+    double height;
+    double depth;
+
+    void volume() {
+        System.out.print("Volume is ");
+        System.out.println(width * height * depth);
+    }
+}
