fe3fc5068ccf2670b46b152274c0de248f4f105b
Vlad Papikyan <vladqot@icloud.com>
2023-01-15 01:15:51 +0300

    commit

--- src/homework/homework7/Calculator.java
+++ src/homework/homework7/Calculator.java
@@ -14,11 +14,10 @@ public class Calculator {
     }
 
     double div(double x, double y) {
-        if (y == 0) {
+        if(y == 0) {
             System.out.println("The number is not divisible to zero");
             return -1;
-        } else {
-            return x / y;
         }
+        return x / y;
     }
 }
