eb30d7881902fb8f1a99f0413f3aec0d7dcc01bc
Vlad Papikyan <vladqot@icloud.com>
2023-02-19 12:27:13 +0300

    commit

--- src/homework/employee/EmployeeDemo.java
+++ src/homework/employee/EmployeeDemo.java
@@ -1,64 +1,170 @@
 package homework.employee;
 
+import homework.employee.model.Employee;
+import homework.employee.storage.EmployeeStorage;
+import homework.employee.util.DateUtil;
+
+import javax.xml.crypto.Data;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class EmployeeDemo {
 
-    static Scanner scanner = new Scanner(System.in);
-    static EmployeeStorage employeeStorage = new EmployeeStorage();
 
-    public static void main(String[] args) {
+    private static Scanner scanner = new Scanner(System.in);
+    private static EmployeeStorage employeeStorage = new EmployeeStorage();
+
+    public static void main(String[] args) throws ParseException {
+
+        employeeStorage.add(new Employee("Poxos", "Poxosyan", "a003", 150, "compani 1", "jr. manager", new Date(), DateUtil.stringToDate("19/03/1985")));
+        employeeStorage.add(new Employee("Varos", "Varosyan", "a002", 250, "compani 1", "manager", new Date(), DateUtil.stringToDate("27/07/1987")));
+        employeeStorage.add(new Employee("Mko", "Mkrtchyan", "a001", 350, "compani 1", "senior manager", new Date(), DateUtil.stringToDate("13/03/1993")));
 
         boolean isRun = true;
 
         while (isRun) {
-            System.out.println("Please input 0 for exit");
-            System.out.println("Please input 1 for add employee");
-            System.out.println("Please input 2 for print all employee");
-            System.out.println("Please input 3 for search employee by employee ID");
-            System.out.println("Please input 4 for search employee by company name");
-            String command = scanner.nextLine();
+            printCommands();
 
+            String command = scanner.nextLine();
             switch (command) {
                 case "0":
                     isRun = false;
                     break;
                 case "1":
-                    createEmployee();
+                    addEmployee();
                     break;
                 case "2":
                     employeeStorage.print();
                     break;
                 case "3":
-                    System.out.println(employeeStorage.searchById());
+                    getEmployeeById();
                     break;
                 case "4":
-                    System.out.println(employeeStorage.searchByCompName());
+                    getEmployeesByCompanyName();
+                    break;
+                case "5":
+                    searchEmployeesBySalaryRange();
+                case "6":
+                    changePositionById();
+                    break;
+                case "7":
+                    employeeStorage.printByStatus(true);
+                    break;
+                case "8":
+                    inactivateEmployee();
+                    break;
+                case "9":
+                    activateEmployee();
                     break;
                 default:
-                    System.out.println("Wrong command. Please try again.");
+                    System.out.println("Wrong command. Please try again!!!");
             }
         }
     }
 
-    static void createEmployee() {
-        System.out.println("plese input employee name");
-        String name = scanner.nextLine();
-        System.out.println("please input employee surname");
-        String surname = scanner.nextLine();
-        System.out.println("please input employee ID");
-        String employeeid = scanner.nextLine();
-        System.out.println("please input employee salary");
-        Double salary = Double.parseDouble(scanner.nextLine());
-        System.out.println("please input employee company name");
-        String company = scanner.nextLine();
-        System.out.println("please input employee position");
-        String position = scanner.nextLine();
-        Employee employee = new Employee(name, surname, employeeid,
-                Double.parseDouble(String.valueOf(salary)), company, position);
-        EmployeeStorage.add(employee);
-        System.out.println("Employee is created");
-        System.out.println();
+    private static void activateEmployee() {
+        employeeStorage.printByStatus(false);
+        System.out.println("please choose employee id");
+        String employeeId = scanner.nextLine();
+        Employee employee = employeeStorage.getEmployeeById(employeeId);
+        if (employee == null || employee.isActive()) {
+            System.out.println("Wrong employee id,or employee is inactive. Please try again!");
+        } else {
+            employee.setActive(true);
+            System.out.println("Status changed!");
+        }
+    }
+
+    private static void inactivateEmployee() {
+        employeeStorage.printByStatus(true);
+        System.out.println("please choose employee id");
+        String employeeId = scanner.nextLine();
+        Employee employee = employeeStorage.getEmployeeById(employeeId);
+        if (employee == null || !employee.isActive()) {
+            System.out.println("Wrong employee id,or employee is inactive. Please try again!");
+        } else {
+            employee.setActive(false);
+            System.out.println("Status changed!");
+        }
+    }
+
+    private static void printCommands() {
+        System.out.println("Please input 0 for exit");
+        System.out.println("Please input 1 for add employee");
+        System.out.println("Please input 2 for print employees");
+        System.out.println("Please input 3 for search employee by id");
+        System.out.println("Please input 4 for search employee by compani name");
+        System.out.println("Please input 5 for search employee by salary range");
+        System.out.println("Please input 6 for change employee position by id");
+        System.out.println("Please input 7 for print only active employees");
+        System.out.println("Please input 8 for inactive employee by id");
+        System.out.println("Please input 9 for active employee by id");
+    }
+
+
+    private static void getEmployeeById() {
+        System.out.println("please input employee Id");
+        String employeeId = scanner.nextLine();
+        Employee employee = employeeStorage.getEmployeeById(employeeId);
+        if (employee == null) {
+            System.out.println("emloyee with " + employeeId + " id dosn't exist");
+        } else {
+            System.out.println(employee);
+        }
+    }
+
+
+    private static void addEmployee() throws ParseException {
+
+        System.out.println("Please input name, surname, employeeId, salary, companyName, position, dateOfBirthday(15/02/2023)");
+        String employeeDataStr = scanner.nextLine();
+        String[] employeeData = employeeDataStr.split(",");
+        String employeeId = employeeData[2];
+        Employee employeeById = employeeStorage.getEmployeeById(employeeId);
+        if (employeeById == null) {
+            Employee employee = new Employee(employeeData[0], employeeData[1], employeeId, Double.parseDouble(employeeData[3]),
+                    employeeData[4], employeeData[5], new Date(), DateUtil.stringToDate(employeeData[6]));
+            employeeStorage.add(employee);
+            System.out.println("Employee was added!");
+        } else {
+            System.out.println("Employee with " + employeeId + " id already exist");
+        }
+    }
+
+    private static void searchEmployeesBySalaryRange() {
+        System.out.println("Please input minPrice, maxPrice");
+        String salaryRangeStr = scanner.nextLine();
+        String[] salaryRange = salaryRangeStr.split(",");
+        double minPrice = Double.parseDouble(salaryRange[0]);
+        double maxPrice = Double.parseDouble(salaryRange[1]);
+        if (maxPrice < minPrice) {
+            System.out.println("minPrise should be less than maxPrice");
+            return;
+        }
+        employeeStorage.searchBySalaryRange(minPrice, maxPrice);
+    }
+
+    private static void getEmployeesByCompanyName() {
+        System.out.println("please input company name");
+        String compamyName = scanner.nextLine();
+        employeeStorage.searchEmployeeByConpanyName(compamyName);
+    }
+
+    private static void changePositionById() {
+        employeeStorage.printByStatus(true);
+        System.out.println("please choose employee id");
+        String employeeId = scanner.nextLine();
+        Employee employee = employeeStorage.getEmployeeById(employeeId);
+        if (employee == null) {
+            System.out.println("Wrong employee id, please try again!");
+        } else {
+            System.out.println("please input new position name");
+            String position = scanner.nextLine();
+            employee.setPosition(position);
+            System.out.println("Position changed!");
+        }
     }
 }
 
---
+++ src/homework/employee/model/Employee.java
@@ -0,0 +1,158 @@
+package homework.employee.model;
+
+import homework.employee.util.DateUtil;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Employee {
+
+    private String name;
+    private String surname;
+    private String employeeId;
+    private double salary;
+    private String company;
+    private String position;
+    private boolean active = true;
+    private Date registerDate;
+    private Date dateOfBirthday;
+
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surname, String employeeId, double salary,
+                    String company, String position, Date registerDate, Date dateOfBirthday) {
+        this.name = name;
+        this.surname = surname;
+        this.employeeId = employeeId;
+        this.salary = salary;
+        this.company = company;
+        this.position = position;
+        this.dateOfBirthday = dateOfBirthday;
+        this.registerDate = registerDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", employeeId='" + employeeId + '\'' +
+                ", salary=" + salary +
+                ", company='" + company + '\'' +
+                ", position='" + position + '\'' +
+                ", active=" + active +
+                ", registerDate=" + DateUtil.dateToString(registerDate) +
+                ", dateOfBirthday=" + DateUtil.dateToString(dateOfBirthday) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (Double.compare(employee.salary, salary) != 0) return false;
+        if (active != employee.active) return false;
+        if (!Objects.equals(name, employee.name)) return false;
+        if (!Objects.equals(surname, employee.surname)) return false;
+        if (!Objects.equals(employeeId, employee.employeeId)) return false;
+        if (!Objects.equals(company, employee.company)) return false;
+        if (!Objects.equals(position, employee.position)) return false;
+        if (!Objects.equals(registerDate, employee.registerDate))
+            return false;
+        return Objects.equals(dateOfBirthday, employee.dateOfBirthday);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (employeeId != null ? employeeId.hashCode() : 0);
+        temp = Double.doubleToLongBits(salary);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (company != null ? company.hashCode() : 0);
+        result = 31 * result + (position != null ? position.hashCode() : 0);
+        result = 31 * result + (active ? 1 : 0);
+        result = 31 * result + (registerDate != null ? registerDate.hashCode() : 0);
+        result = 31 * result + (dateOfBirthday != null ? dateOfBirthday.hashCode() : 0);
+        return result;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public Date getDateOfBirthday() {
+        return dateOfBirthday;
+    }
+
+    public void setDateOfBirthday(Date dateOfBirthday) {
+        this.dateOfBirthday = dateOfBirthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
